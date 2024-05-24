2d1e2918d9242efcbc8f60ffce2c217fc5762273
TanVu <tanvnph33606@gmail.com>
2024-05-24 19:41:11 +0700

    Update ProductCatalogueController

--- app/Http/Controllers/Clients/ProductCatalogueController.php
+++ app/Http/Controllers/Clients/ProductCatalogueController.php
@@ -4,25 +4,39 @@ namespace App\Http\Controllers\Clients;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Interfaces\ProductCatalogueRepositoryInterface as ProductCatalogueRepository;
+use App\Services\Interfaces\ProductServiceInterface as ProductService;
+
+use App\Services\Interfaces\ProductCatalogueServiceInterface as ProductCatalogueService;
+
+
 use Illuminate\Http\Request;
 
 class ProductCatalogueController extends Controller
 {
     private $productCatalogueRepository;
+    private $productService;
     public function __construct()
     {
         parent::__construct();
         $this->productCatalogueRepository = app(ProductCatalogueRepository::class);
+        $this->productService = app(ProductService::class);
     }
     public function index($id)
     {
         $productCatalogue = $this->productCatalogueRepository->getProductCatalogueLanguageById($id, session('currentLanguage') ?? 1);
-
+        $breadcrumb = $this->productCatalogueRepository->breadcrumb($productCatalogue, session('currentLanguage') ?? 1);
+        $products = $this->productService->paginate($productCatalogue);
+        $productId = $products->pluck('id')->toArray();
+        if (count($productId) > 0) {
+            $products = $this->productService->combineProductAndPromotion($productId, $products);
+        }
         $seo = seo($productCatalogue);
 
         return view('clients.product_catalogues.index', compact(
+            'seo',
+            'breadcrumb',
             'productCatalogue',
-            'seo'
+            'products',
         ));
     }
 }
