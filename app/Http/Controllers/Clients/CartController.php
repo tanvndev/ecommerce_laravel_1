2812484ef7d85725e79bfb3a60d1e8632bd73d23
TanVu <tanvnph33606@gmail.com>
2024-05-26 14:28:45 +0700

    Add new Cart Controller

---
+++ app/Http/Controllers/Clients/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Clients;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function checkout()
+    {
+    }
+}
