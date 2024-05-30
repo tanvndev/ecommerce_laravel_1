eb0608f22d709e0f28d1e0303f5bc73129590bcf
TanVu <tanvnph33606@gmail.com>
2024-05-30 15:13:12 +0700

    Add new orders table

--- database/migrations/2024_05_30_150041_create_orders_table.php
+++ database/migrations/2024_05_30_150041_create_orders_table.php
@@ -13,9 +13,29 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->string('fullname');
+            $table->string('phone', 20);
+            $table->string('email', 50);
+            $table->string('province_id', 10)->nullable();
+            $table->string('district_id', 10)->nullable();
+            $table->string('ward_id', 10)->nullable();
+            $table->string('address');
+            $table->string('payment_method');
+            $table->string('description')->nullable();
+            $table->json('promotion')->nullable();
+            $table->json('cart')->nullable();
+            $table->unsignedBigInteger('customer_id');
+            $table->string('guest_cookie')->nullable();
+            $table->string('confirm', 20);
+            $table->string('payment', 20);
+            $table->string('delevery', 20);
+            $table->double('shipping')->default(0);
             $table->timestamps();
         });
     }
+
+
 
     /**
      * Reverse the migrations.
